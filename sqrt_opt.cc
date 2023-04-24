013003a1766fe81d7749354190fc86b51c93dfaf
Ansgar Lichter <lichteransgar@gmail.com>
2023-04-24 18:48:45 +0200

    feat(task2): implementation

--- sqrt_opt.cc
+++ sqrt_opt.cc
@@ -1,7 +1,7 @@
 // g++ -Wall -pedantic -march=native -mfpmath=sse -msse -O3 sqrt_opt.cc
 // für objdump:
 // g++ -Wall -pedantic -march=native -mfpmath=sse -msse -O3 sqrt_opt.cc -g -c
-// objdump -S sqrt_sse.o 
+// objdump -S sqrt_sse.o
 #include <iostream>
 #include <limits>
 #include <random>
@@ -16,86 +16,130 @@ double random_double()
 }
 
 template <size_t LOOPS = 2>
-void measure_sqrt_time(void) {
-    const static int LOOP = 1000;
-    const static int N = 100000;
-    MeasureTime<std::chrono::nanoseconds> time;
-    alignas(128) float floats[N * 4];
-    alignas(128) float roots[N * 4];
+void measure_sqrt_time(void)
+{
+  const static int LOOP = 1000;
+  const static int N = 100000;
+  MeasureTime<std::chrono::nanoseconds> time;
+  alignas(128) float floats[N * 4];
+  alignas(128) float roots[N * 4];
 
-    std::cout << LOOPS  << " iterations" << std::endl;
-    std::cout << "generating " << (4 * N) << " random values..." << std::endl;
-    for (int i = 0; i < 4 * N; i++) {
-       floats[i] = random_double();
+  std::cout << LOOPS << " iterations" << std::endl;
+  std::cout << "generating " << (4 * N) << " random values..." << std::endl;
+  for (int i = 0; i < 4 * N; i++)
+  {
+    floats[i] = random_double();
+  }
+
+  std::cout << "math sqrt" << std::endl;
+  time.start_clock();
+  for (int j = 0; j < LOOP; j++)
+  {
+    for (int i = 0; i < 4 * N; i += 4)
+    {
+      for (int k = 0; k < 4; k++)
+      {
+        roots[i + k] = sqrt(floats[i + k]);
+      }
     }
+  }
+  time.stop_clock();
+  std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
+  time.reset_clock();
 
-    std::cout << "math sqrt" << std::endl;
-    time.start_clock();
-    for (int j = 0; j < LOOP; j++) {
-      for (int i = 0; i < 4 * N; i += 4) {
-        for (int k = 0; k < 4; k++) {
-          roots[i + k] = sqrt(floats[i + k]);
-        }
-      }    
+  std::cout << "sqrt1 (Newton method for single float, one time a loop)" << std::endl;
+  time.start_clock();
+  for (int j = 0; j < LOOP; j++)
+  {
+    for (int i = 0; i < 4 * N; i++)
+    {
+      roots[i] = sqrt1<LOOPS>(floats + i);
     }
-    time.stop_clock();
-    std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
-    time.reset_clock();
+  }
+  time.stop_clock();
+  std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
+  time.reset_clock();
 
-    std::cout << "sqrt1 (Newton method for single float, one time a loop)" << std::endl;
-    time.start_clock();
-    for (int j = 0; j < LOOP; j++) {
-      for (int i = 0; i < 4 * N; i++) {
-        roots[i] = sqrt1<LOOPS>(floats + i);
+  std::cout << "sqrt1 (Newton method for single float, four times a loop)" << std::endl;
+  time.start_clock();
+  for (int j = 0; j < LOOP; j++)
+  {
+    for (int i = 0; i < 4 * N; i += 4)
+    {
+      for (int k = 0; k < 4; k++)
+      {
+        roots[i + k] = sqrt1<LOOPS>(floats + i + k);
       }
-    }    
-    time.stop_clock();
-    std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
-    time.reset_clock();
+    }
+  }
+  time.stop_clock();
+  std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
+  time.reset_clock();
 
+  std::cout << "sqrt2 (Newton method for sequence of 4 floats)" << std::endl;
+  time.start_clock();
+  for (int j = 0; j < LOOP; j++)
+  {
+    for (int i = 0; i < 4 * N; i += 4)
+    {
+      sqrt2<LOOPS>(floats + i, roots + i);
+    }
+  }
+  time.stop_clock();
+  std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
+  time.reset_clock();
 
-    std::cout << "sqrt1 (Newton method for single float, four times a loop)" << std::endl;
-    time.start_clock();
-    for (int j = 0; j < LOOP; j++) {
-      for (int i = 0; i < 4 * N; i += 4) {
-        for (int k = 0; k < 4; k++) {
-          roots[i + k] = sqrt1<LOOPS>(floats + i + k);
-        }
-      }
-    }    
-    time.stop_clock();
-    std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
-    time.reset_clock();
+  time.start_clock();
+  for (int j = 0; j < LOOP; j++)
+  {
+    for (int i = 0; i < 4 * N; i += 4)
+    {
+      sqrt3<LOOPS>(floats + i, roots + i);
+    }
+  }
+  time.stop_clock();
 
-    std::cout << "sqrt2 (Newton method for sequence of 4 floats)" << std::endl;
-    time.start_clock();
-    for (int j = 0; j < LOOP; j++) {
-      for (int i = 0; i < 4 * N; i += 4) {
-        sqrt2<LOOPS>(floats + i, roots + i);
-      }
-    }    
-    time.stop_clock();
-    std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
-    time.reset_clock();
-
-    time.start_clock();
-    for (int j = 0; j < LOOP; j++) {
-      for (int i = 0; i < 4 * N; i += 4) {
-        sqrt3<LOOPS>(floats + i, roots + i);
-      }
-    }    
-    time.stop_clock();
-
-    std::cout << "sqrt3 (Newton method for sequence of 4 floats, SIMD)" << std::endl;
-    std::cout << time.time_clock().count() / LOOP  << " [ns]" << std::endl;
-    time.reset_clock();
+  std::cout << "sqrt3 (Newton method for sequence of 4 floats, SIMD)" << std::endl;
+  std::cout << time.time_clock().count() / LOOP << " [ns]" << std::endl;
+  time.reset_clock();
 }
 
-int main(void) {
-    measure_sqrt_time<2>();
-    // uncomment next lines for disassembly
-    measure_sqrt_time<3>();
-    measure_sqrt_time<4>();
-    return 1;
-}
+int main(void)
+{
+  /* float values[4] = {
+    4.0,
+    9.0,
+    16.0,
+    25.0
+  };
+  float root[4];
+  float root2[4];
 
+  std::cout << "sqrt1:" << std::endl;
+  for (int i = 0; i < 4; i++)
+  {
+    std::cout << values[i] << " " << sqrt1<2>(values + i) << std::endl;
+  } */
+
+  /* std::cout << "sqrt2:" << std::endl;
+  sqrt2<2>(values, root);
+  for (int i = 0; i < 4; i++)
+  {
+    std::cout << values[i] << " " << root[i] << std::endl;
+  }
+  
+  std::cout << "sqrt3:" << std::endl;
+  sqrt3<2>(values, root2);
+  for (int i = 0; i < 4; i++)
+  {
+    std::cout << values[i] << " " << root2[i] << std::endl;
+  } */
+
+
+
+  measure_sqrt_time<2>();
+  // uncomment next lines for disassembly
+  measure_sqrt_time<3>();
+  measure_sqrt_time<4>();
+  return 1;
+}
